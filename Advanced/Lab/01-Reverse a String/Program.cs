a286813d96e2c865a54679089900a21850a9eec3
Lanber <ersinengels94@gmail.com>
2026-02-23 20:48:30 +0100

    Advanced

---
+++ Advanced/Lab/01-Reverse a String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _1._Reverse_a_String
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char symbol in text)
+            {
+                stack.Push(symbol);
+            }
+
+            while (stack.Count > 0)
+            {
+                var symbol = stack.Pop();
+                Console.Write(symbol);
+            }
+        }
+    }
+}
