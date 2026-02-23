b8c6196970b85a76e5962b13b6e7770cd0339446
Lanber <ersinengels94@gmail.com>
2026-02-24 00:04:35 +0100

    Advanced

--- Advanced/Lab/03-Sumple Calculator/Program.cs
+++ Advanced/Lab/03-Sumple Calculator/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var tokens = Console.ReadLine().Split().Reverse().ToArray();
+
+            var stack = new Stack<string>(tokens);
+
+            bool isAddition = true;
+
+            int sum = 0;
+
+            while(stack.Count > 0)
+            {
+                var currenToken = stack.Pop();
+
+                if (currenToken == "+")
+                {
+                    isAddition = true ;
+                }
+                else if(currenToken == "-")
+                {
+                    isAddition = false ;
+                }
+                else
+                {
+                    int number = int.Parse(currenToken);
+
+                    if(!isAddition )
+                    {
+                        number *= -1;
+                    }
+
+                    sum += number;
+                }
+            }
+            Console.WriteLine(sum);
         }
     }
 }
