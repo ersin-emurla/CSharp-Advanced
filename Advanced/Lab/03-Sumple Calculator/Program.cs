0e6c8dc44646b8af224b88ccd081464981122f62
Lanber <ersinengels94@gmail.com>
2026-02-24 00:00:49 +0100

    Advanced

---
+++ Advanced/Lab/03-Sumple Calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03_Sumple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
