be44422516f87f69ee82783ec740f047b5913f81
Lanber <ersinengels94@gmail.com>
2026-02-24 22:22:13 +0100

    Advanced

---
+++ Advanced/Lab/04-Matching Brackets/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04_Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exporession = Console.ReadLine();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < exporession.Length; i++)
+            {
+                var symbol = exporession[i];
+
+                if (symbol == '(')
+                {
+                    stack.Push(i);
+                }
+
+                else if (symbol == ')')
+                {
+                    var openingBracketIndex = stack.Pop();
+                    var closingBracketIndex = i; 
+
+                    var result = exporession.Substring(openingBracketIndex, closingBracketIndex - openingBracketIndex + 1);
+
+                    Console.WriteLine(result);
+
+
+                }
+            }
+        }
+    }
+}
