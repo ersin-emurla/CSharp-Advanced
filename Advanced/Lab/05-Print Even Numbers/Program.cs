106fa1b9097cc18576150042e4670fe06efda69c
Lanber <ersinengels94@gmail.com>
2026-02-25 23:47:40 +0100

    Advanced

---
+++ Advanced/Lab/05-Print Even Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _05_Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var queue = new Queue<int>(numbers);  
+            var result =new List<int>();
+
+          
+      
+
+            while (queue.Count > 0)
+            {
+                var number = queue.Dequeue();
+
+                if (number % 2 == 0)
+                {
+                    result.Add(number);  
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        
+
+        }
+    }
+}
