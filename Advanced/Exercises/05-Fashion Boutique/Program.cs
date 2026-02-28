5b504e76d1ca146a51cb882eb41c3b058c15b1d4
Lanber <ersinengels94@gmail.com>
2026-03-01 00:06:49 +0100

    Advanced

---
+++ Advanced/Exercises/05-Fashion Boutique/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _05_Fashion_Boutique;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<int> clothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+        int capacity =int.Parse(Console.ReadLine());
+
+        int racks = 0, free = 0;
+        while (clothes.Count > 0)
+        {
+            int currentElements = clothes.Pop();
+
+            if (free < currentElements)
+            {
+                racks++;
+                free = capacity;
+            }
+
+            free -= currentElements;
+        }
+
+        Console.WriteLine(racks);
+    }
+}
