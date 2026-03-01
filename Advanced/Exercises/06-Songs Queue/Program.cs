7d327e57d17d95db3d14bf7bd0bf79003fba7f5c
Lanber <ersinengels94@gmail.com>
2026-03-01 22:54:40 +0100

    Advanced

---
+++ Advanced/Exercises/06-Songs Queue/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace _06_Songs_Queue;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+       Queue<string> queue = new Queue<string>();
+       HashSet<string> uniqueNames = new HashSet<string>();
+
+        string[] initialSongs = Console.ReadLine().Split(", ");
+        foreach (string song in initialSongs)
+        {
+          if (uniqueNames.Add(song)) 
+            queue.Enqueue(song);
+        }
+
+        while (queue.Count > 0)
+        {
+            string[] data = Console.ReadLine().Split();
+            string command = data[0];
+
+            if (command == "Add")
+            {
+                string song = string.Join(' ', data.Skip(1));
+
+                // TODO: Extract a method for duplicated code.
+                if (uniqueNames.Add(song))
+                    queue.Enqueue(song);
+                else
+                    Console.WriteLine($"{song} is already contained!");
+                
+            }
+            else if (command == "Play")
+            {
+                string removedSong = queue.Dequeue();
+                uniqueNames.Remove(removedSong);
+            }
+            else if (command == "Show")
+            {
+                Console.WriteLine(string.Join(", ", queue));
+            }
+        }
+        Console.WriteLine("No more songs!");
+
+    }
+}
